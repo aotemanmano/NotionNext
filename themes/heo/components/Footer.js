2125866a1a61225cd6da54395efcfcabe43e587c
Anunaki <31979593+aotemanmano@users.noreply.github.com>
2025-12-02 11:17:58 +0800

    Refactor footer layout and structure

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -27,7 +27,10 @@ const Footer = () => {
       <br />
 
       {/* 底部页面信息 */}
-     <div id='footer-bottom-left' className='text-center lg:text-start'>
+      <div
+        id='footer-bottom'
+        className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
+        <div id='footer-bottom-left' className='text-center lg:text-start'>
           <div className='flex gap-x-1'>
             <CopyRightDate />
             <a
